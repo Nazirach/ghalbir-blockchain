59d84794fd87f0ea7d2e63e91fd0b50352d7693e
Nazirach <goomevision@gmail.com>
2025-04-24 01:58:38 +0700

    ghalbitblockchai

--- mine.py
+++ mine.py
@@ -6,6 +6,19 @@ import json
 import time
 import argparse
 from src.core.blockchain import Blockchain
+import firebase_admin
+from firebase_admin import auth, credentials
+
+cred = credentials.Certificate("serviceAccountKey.json")  # Download dari Firebase Console
+firebase_admin.initialize_app(cred)
+
+def verify_firebase_token(id_token: str):
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+        return uid
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Token tidak valid")
 
 def main():
     parser = argparse.ArgumentParser(description='Mining Ghalbir Blockchain')
